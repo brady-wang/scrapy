4fc73dcf61eafbb661675a64deaa82a96a00e125
王勇顺 <brady.wang@qq.com>
2019-06-12 15:03:37 +0800

    d

--- spider/quote/quote/spiders/first.py
+++ spider/quote/quote/spiders/first.py
@@ -17,20 +17,24 @@ class FirstSpider(scrapy.Spider):
             yield scrapy.Request(url=url,callback=self.parse)
 
     def parse(self, response):
-        items = response.xpath("//div[@class='quote']").extract()
-        with open("data.html",'a',encoding='utf-8') as f:
-            for quote in items:
-                text = Selector(text=quote).xpath("//span[@class='text']/text()").extract_first()
-                author = Selector(text=quote).xpath("//small[@class='author']/text()").extract_first()
-                f.write(text)
-                f.write('\n')
-                f.write("作者："+ author)
-                f.write('\n-------------------------\n')
-                print(text)
-        print("crawl page " + response.url)
-        nextPage = response.xpath("/html/body/div/div[2]/div[1]/nav/ul/li[@class='next']/a/@href").extract_first()
-        if nextPage is not None:
-            url = response.urljoin(nextPage)
-            yield scrapy.Request(url=url,callback=self.parse)
+        try:
 
-        f.close()
+            items = response.xpath("//div[@class='quote']").extract()
+            with open("data.html",'a',encoding='utf-8') as f:
+                for quote in items:
+                    text = Selector(text=quote).xpath("//span[@class='text']/text()").extract_first()
+                    author = Selector(text=quote).xpath("//small[@class='author']/text()").extract_first()
+                    f.write(text)
+                    f.write('\n')
+                    f.write("作者："+ author)
+                    f.write('\n-------------------------\n')
+                    print(text)
+            print("crawl page " + response.url)
+            nextPage = response.xpath("/html/body/div/div[2]/div[1]/nav/ul/li[@class='next']/a/@href").extract_first()
+            if nextPage is not None:
+                url = response.urljoin(nextPage)
+                yield scrapy.Request(url=url,callback=self.parse)
+
+            f.close()
+        except Exception as f:
+            print(f)
